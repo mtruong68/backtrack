d2cb7826ed7231b0f26d7f204feeeeea64db0fc1
u3547052 <56960821+u3547052@users.noreply.github.com>
2019-10-25 11:57:30 +0800

    Remove redundant files

--- William/backtrack/Scripts/django-admin.py
+++
@@ -1,7 +0,0 @@
-#!c:\users\wlcheung\envs\backtrack\scripts\python.exe
-# EASY-INSTALL-DEV-SCRIPT: 'Django==3.1','django-admin.py'
-__requires__ = 'Django==3.1'
-__import__('pkg_resources').require('Django==3.1')
-__file__ = 'C:\\Users\\wlcheung\\Envs\\backtrack\\django\\django\\bin\\django-admin.py'
-with open(__file__) as f:
-    exec(compile(f.read(), __file__, 'exec'))
