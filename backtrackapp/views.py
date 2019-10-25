1097266eec4c4048108b163a426efae7cf46b29d
u3547052 <56960821+u3547052@users.noreply.github.com>
2019-10-26 06:36:14 +0800

    Modify and Refine PBI
    Modify and Refine PBI with basic CSS styling

--- backtrackapp/views.py
+++ backtrackapp/views.py
@@ -60,6 +60,8 @@ class ProjectPBView(generic.CreateView):
             return self.createNewPBI(request, pk)
         if 'deletePBI' in self.request.POST:
             return self.deletePBI(request, pk)
+        if 'splitPBI' in self.request.POST:
+            return self.splitPBI(request, pk)
         else:
             #this is a stub method and needs to be changed
             print(form.errors)
@@ -91,6 +93,19 @@ class ProjectPBView(generic.CreateView):
             #this is a stub method and needs to be changed
             print(form.errors)
             return HttpResponse("Did not work.")
+
+    def splitPBI(self, request, pk):
+        proj = get_object_or_404(Project, pk=pk)
+        num = request.POST.get('numOfChildPBI')
+        pbi_id = request.POST.get('pbi')
+        pbi = ProductBacklogItem.objects.get(pk=pbi_id)
+        i = 1
+        while (i <= int(num)):
+            ProductBacklogItem.objects.create(name = pbi.name + "." + str(i), desc = pbi.desc, priority = pbi.priority, storypoints = pbi.storypoints, status = pbi.status, project = proj)
+            i = i + 1
+        ProductBacklogItem.objects.get(pk=pbi_id).delete()
+        #redirect back to product backlog view
+        return HttpResponseRedirect(reverse('backtrack:project_pb', args=(pk,)))
 
 #Views handling the client accessing the Sprint Backlog
 #NOTE THIS IS NOT YET "COMPLETE" FORMS MUST BE INCLUDED
@@ -128,6 +143,8 @@ class SprintBacklogView(generic.DetailView):
 
 
 
+
+
 #Views handling the client accessing the Product Backlog
 #Maybe make separate view for just looking at the product backlog
 class modifyPBI(generic.CreateView):
@@ -139,7 +156,6 @@ class modifyPBI(generic.CreateView):
         return render(request, 'backtrackapp/modifyPBI.html', context)
 
     def post(self, request, pk):
-
         if 'savePBI' in self.request.POST:
             return self.savePBI(request, pk)
         else:
@@ -157,31 +173,3 @@ class modifyPBI(generic.CreateView):
         pbi.status = request.POST.get('newSta')
         pbi.save()
         return HttpResponseRedirect(reverse('backtrack:modifyPBI', args=(pk,)))
-
-
-    # def addToSprint(self, request, pk):
-    #     latestSprint = Sprint.objects.latest('start_date')
-    #     pbi_id = request.POST.get('pbi')
-    #     pbi = get_object_or_404(ProductBacklogItem, pk=pbi_id)
-    #     pbi.sprint = latestSprint
-    #     pbi.save()
-    #     return HttpResponseRedirect(reverse('backtrack:project_pb', args=(pk,)))
-    #
-    # def deletePBI(self, request, pk):
-    #     pbi_id = request.POST.get('pbi')
-    #     ProductBacklogItem.objects.get(pk=pbi_id).delete()
-    #     return HttpResponseRedirect(reverse('backtrack:project_pb', args=(pk,)))
-    #
-    # def createNewPBI(self, request, pk):
-    #     project = get_object_or_404(Project, pk=pk)
-    #     form = NewPBIForm(request.POST, initial={'project': project})
-    #     if form.is_valid():
-    #         newPBI = form.save(commit=False)
-    #         newPBI.project = project
-    #         newPBI.save()
-    #         #redirect back to product backlog view
-    #         return HttpResponseRedirect(reverse('backtrack:project_pb', args=(pk,)))
-    #     else:
-    #         #this is a stub method and needs to be changed
-    #         print(form.errors)
-    #         return HttpResponse("Did not work.")
